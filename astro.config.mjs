fbc535d31b22b310b4fb5ea96d7a7916bf9f6727
arsani <arsanig@users.noreply.github.com>
2026-03-12 20:02:33 -0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [
+            tailwindcss(),
+            paraglideVitePlugin({
+                project: "./project.inlang",
+                outdir: "./src/paraglide",
+            }),
+        ],
+    },
+    output: "static",
+    adapter: node({ mode: "standalone" }),
+});
