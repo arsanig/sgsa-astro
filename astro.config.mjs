cc7f6f06466da8410c5a8621837943ac946f77ec
arsani <arsanig@users.noreply.github.com>
2026-03-14 20:10:13 -0400

    fix: nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
 
 // https://astro.build/config
